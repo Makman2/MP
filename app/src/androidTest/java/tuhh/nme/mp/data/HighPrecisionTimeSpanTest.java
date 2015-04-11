c12d7809e89736e3311d6edeeb82137d4ecc8611
Makman2 <mak95@arcor.de>
2015-04-11 21:21:06 +0200

    data: Add HighPrecisionTimeSpan
    Represents a time span with nanosecond-precision.

---
+++ app/src/androidTest/java/tuhh/nme/mp/data/HighPrecisionTimeSpanTest.java
@@ -0,0 +1,109 @@
+package tuhh.nme.mp.data;
+
+import android.test.AndroidTestCase;
+
+public class HighPrecisionTimeSpanTest extends AndroidTestCase
+{
+    /**
+     * Tests the construction of an HighPrecisionTimeSpan.
+     */
+    public void testConstruction()
+    {
+        HighPrecisionTimeSpan uut;
+        uut = new HighPrecisionTimeSpan(BigIntegerConverter.toBigInteger(0));
+        assertEquals(uut.getTime().longValue(), 0L);
+        uut = new HighPrecisionTimeSpan(BigIntegerConverter.toBigInteger(1000));
+        assertEquals(uut.getTime().longValue(), 1000L);
+        uut = new HighPrecisionTimeSpan(BigIntegerConverter.toBigInteger(21987L));
+        assertEquals(uut.getTime().longValue(), 21987L);
+        uut = new HighPrecisionTimeSpan(BigIntegerConverter.toBigInteger(Long.MAX_VALUE));
+        assertEquals(uut.getTime().longValue(), Long.MAX_VALUE);
+
+        HighPrecisionTimeSpan uut2 = new HighPrecisionTimeSpan(uut);
+        assertEquals(uut.getTime(), uut2.getTime());
+        assertNotSame(uut, uut2);
+    }
+
+    /**
+     * Tests the static constructor functions.
+     */
+    public void testStaticConstructors()
+    {
+        HighPrecisionTimeSpan uut;
+
+        // From nanoseconds.
+        uut = HighPrecisionTimeSpan.fromNanoseconds(0L);
+        assertEquals(uut.getTime().longValue(), 0L);
+        uut = HighPrecisionTimeSpan.fromNanoseconds(100L);
+        assertEquals(uut.getTime().longValue(), 100L);
+        uut = HighPrecisionTimeSpan.fromNanoseconds(82710L);
+        assertEquals(uut.getTime().longValue(), 82710L);
+
+        // From microseconds.
+        uut = HighPrecisionTimeSpan.fromMicroseconds(0L);
+        assertEquals(uut.getTime().longValue(), 0L);
+        uut = HighPrecisionTimeSpan.fromMicroseconds(217L);
+        assertEquals(uut.getTime().longValue(), 217L * 1000L);
+        uut = HighPrecisionTimeSpan.fromMicroseconds(777L);
+        assertEquals(uut.getTime().longValue(), 777L * 1000L);
+
+        // From milliseconds.
+        uut = HighPrecisionTimeSpan.fromMilliseconds(0L);
+        assertEquals(uut.getTime().longValue(), 0L);
+        uut = HighPrecisionTimeSpan.fromMilliseconds(45L);
+        assertEquals(uut.getTime().longValue(), 45L * 1000L * 1000L);
+        uut = HighPrecisionTimeSpan.fromMilliseconds(918);
+        assertEquals(uut.getTime().longValue(), 918L * 1000L * 1000L);
+
+        // From seconds.
+        uut = HighPrecisionTimeSpan.fromSeconds(0L);
+        assertEquals(uut.getTime().longValue(), 0L);
+        uut = HighPrecisionTimeSpan.fromSeconds(1111L);
+        assertEquals(uut.getTime().longValue(), 1111L * 1000L * 1000L * 1000L);
+        uut = HighPrecisionTimeSpan.fromSeconds(217);
+        assertEquals(uut.getTime().longValue(), 217L * 1000L * 1000L * 1000L);
+
+        // From minutes.
+        uut = HighPrecisionTimeSpan.fromMinutes(0L);
+        assertEquals(uut.getTime().longValue(), 0L);
+        uut = HighPrecisionTimeSpan.fromMinutes(918L);
+        assertEquals(uut.getTime().longValue(), 918L * 1000L * 1000L * 1000L * 60L);
+        uut = HighPrecisionTimeSpan.fromMinutes(31);
+        assertEquals(uut.getTime().longValue(), 31L * 1000L * 1000L * 1000L * 60L);
+
+        // From hours.
+        uut = HighPrecisionTimeSpan.fromHours(0L);
+        assertEquals(uut.getTime().longValue(), 0L);
+        uut = HighPrecisionTimeSpan.fromHours(11L);
+        assertEquals(uut.getTime().longValue(), 11L * 1000L * 1000L * 1000L * 60L * 60L);
+        uut = HighPrecisionTimeSpan.fromHours(9918L);
+        assertEquals(uut.getTime().longValue(), 9918L * 1000L * 1000L * 1000L * 60L * 60L);
+    }
+
+    /**
+     * Tests the equal() function.
+     */
+    public void testEqual()
+    {
+        HighPrecisionTimeSpan uut;
+        HighPrecisionTimeSpan uut2;
+
+        uut = HighPrecisionTimeSpan.fromMilliseconds(100);
+        assertEquals(uut, uut);
+
+        uut2 = HighPrecisionTimeSpan.fromMilliseconds(100);
+        assertEquals(uut, uut2);
+
+        uut2 = HighPrecisionTimeSpan.fromHours(1);
+        assertNotSame(uut, uut2);
+    }
+
+    /**
+     * Tests the final zero field.
+     */
+    public void testZeroField()
+    {
+        // The actual value.
+        assertEquals(HighPrecisionTimeSpan.zero.getTime().longValue(), 0);
+    }
+}
---
+++ app/src/main/java/tuhh/nme/mp/data/HighPrecisionTimeSpan.java
@@ -0,0 +1,136 @@
+package tuhh.nme.mp.data;
+
+import java.math.BigInteger;
+
+/**
+ * Represents a time difference with nanosecond-precision.
+ */
+public class HighPrecisionTimeSpan
+{
+    /**
+     * Instantiates a new HighPrecisionTimeSpan from the given count of nanoseconds.
+     *
+     * @param nanoseconds The nanoseconds to initialize with.
+     */
+    public HighPrecisionTimeSpan(BigInteger nanoseconds)
+    {
+        m_Nanoseconds = nanoseconds;
+    }
+
+    /**
+     * Copy constructor.
+     *
+     * @param copy Object where to copy from.
+     */
+    public HighPrecisionTimeSpan(HighPrecisionTimeSpan copy)
+    {
+        this(copy.getTime());
+    }
+
+    /**
+     * Instantiates a new HighPrecisionTimeSpan from the given count of nanoseconds.
+     *
+     * @param nanoseconds The nanoseconds to initialize with.
+     */
+    public static HighPrecisionTimeSpan fromNanoseconds(long nanoseconds)
+    {
+        return new HighPrecisionTimeSpan(BigIntegerConverter.toBigInteger(nanoseconds));
+    }
+
+    /**
+     * Instantiates a new HighPrecisionTimeSpan from the given count of microseconds.
+     *
+     * @param microseconds The microseconds to initialize with.
+     */
+    public static HighPrecisionTimeSpan fromMicroseconds(long microseconds)
+    {
+        BigInteger factor = BigIntegerConverter.toBigInteger(1000);
+        BigInteger time = BigIntegerConverter.toBigInteger(microseconds);
+        return new HighPrecisionTimeSpan(time.multiply(factor));
+    }
+
+    /**
+     * Instantiates a new HighPrecisionTimeSpan from the given count of milliseconds.
+     *
+     * @param milliseconds The milliseconds to initialize with.
+     */
+    public static HighPrecisionTimeSpan fromMilliseconds(long milliseconds)
+    {
+        BigInteger factor = BigIntegerConverter.toBigInteger(1000000);
+        BigInteger time = BigIntegerConverter.toBigInteger(milliseconds);
+        return new HighPrecisionTimeSpan(time.multiply(factor));
+    }
+
+    /**
+     * Instantiates a new HighPrecisionTimeSpan from the given count of seconds.
+     *
+     * @param seconds The seconds to initialize with.
+     */
+    public static HighPrecisionTimeSpan fromSeconds(long seconds)
+    {
+        BigInteger factor = BigIntegerConverter.toBigInteger(1000000000);
+        BigInteger time = BigIntegerConverter.toBigInteger(seconds);
+        return new HighPrecisionTimeSpan(time.multiply(factor));
+    }
+
+    /**
+     * Instantiates a new HighPrecisionTimeSpan from the given count of minutes.
+     *
+     * @param minutes The minutes to initialize with.
+     */
+    public static HighPrecisionTimeSpan fromMinutes(long minutes)
+    {
+        BigInteger factor = BigIntegerConverter.toBigInteger(60000000000L);
+        BigInteger time = BigIntegerConverter.toBigInteger(minutes);
+        return new HighPrecisionTimeSpan(time.multiply(factor));
+    }
+
+    /**
+     * Instantiates a new HighPrecisionTimeSpan from the given count of hours.
+     *
+     * @param hours The hours to initialize with.
+     */
+    public static HighPrecisionTimeSpan fromHours(long hours)
+    {
+        BigInteger factor = BigIntegerConverter.toBigInteger(3600000000000L);
+        BigInteger time = BigIntegerConverter.toBigInteger(hours);
+        return new HighPrecisionTimeSpan(time.multiply(factor));
+    }
+
+    /**
+     * Returns the amount of time stored (in nanoseconds) in this HighPrecisionTimeSpan.
+     *
+     * @return The number of nanoseconds.
+     */
+    public BigInteger getTime()
+    {
+        return m_Nanoseconds;
+    }
+
+    /**
+     * Checks whether the stored time spans of this and the other object do equal.
+     *
+     * @param o The other object to check.
+     * @return  true if equal, false if not.
+     */
+    @Override
+    public boolean equals(Object o)
+    {
+        if (o == null) return false;
+        if (o == this) return true;
+        if (!(o instanceof HighPrecisionTimeSpan)) return false;
+
+        HighPrecisionTimeSpan oHighPrecisionTimeSpan = (HighPrecisionTimeSpan)o;
+        return oHighPrecisionTimeSpan.getTime().equals(m_Nanoseconds);
+    }
+
+    /**
+     * A zero time span.
+     */
+    public static final HighPrecisionTimeSpan zero = HighPrecisionTimeSpan.fromNanoseconds(0);
+
+    /**
+     * The number of nanoseconds stored.
+     */
+    private BigInteger m_Nanoseconds;
+}
