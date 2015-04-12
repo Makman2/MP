49cbd92fa2197925ee521127477d352b5fe2a325
Makman2 <mak95@arcor.de>
2015-04-12 23:41:25 +0200

    data: Add specific PressureDataFrame
    This class will store and parse directly the incomming remote module
    data.

---
+++ app/src/androidTest/java/tuhh/nme/mp/data/PressureDataFrameTest.java
@@ -0,0 +1,136 @@
+package tuhh.nme.mp.data;
+
+import android.test.AndroidTestCase;
+
+import java.util.ArrayList;
+
+public class PressureDataFrameTest extends AndroidTestCase
+{
+    /**
+     * Tests the interval constructor that associates dates with the given data list.
+     */
+    public void testAutoIntervalConstructor()
+    {
+        PressureDataFrame uut;
+
+        ArrayList<Float> testdata = new ArrayList<>();
+        testdata.add(0.0f);
+        testdata.add(1.0f);
+        testdata.add(2.0f);
+        testdata.add(3.0f);
+        testdata.add(77.0f);
+        testdata.add(-129.7f);
+
+        // Test relatively simple behaviour.
+        uut = new PressureDataFrame(testdata,
+                                    HighPrecisionDate.epoch,
+                                    HighPrecisionTimeSpan.fromMilliseconds(1));
+
+        ArrayList<DataPoint<HighPrecisionDate, Float>> comparison = new ArrayList<>();
+        comparison.add(new DataPoint<>(HighPrecisionDate.epoch, 0.0f));
+        comparison.add(new DataPoint<>(HighPrecisionDate.epoch.add(
+                           HighPrecisionTimeSpan.fromMilliseconds(1)), 1.0f));
+        comparison.add(new DataPoint<>(HighPrecisionDate.epoch.add(
+                           HighPrecisionTimeSpan.fromMilliseconds(2)), 2.0f));
+        comparison.add(new DataPoint<>(HighPrecisionDate.epoch.add(
+                           HighPrecisionTimeSpan.fromMilliseconds(3)), 3.0f));
+        comparison.add(new DataPoint<>(HighPrecisionDate.epoch.add(
+                           HighPrecisionTimeSpan.fromMilliseconds(4)), 77.0f));
+        comparison.add(new DataPoint<>(HighPrecisionDate.epoch.add(
+                           HighPrecisionTimeSpan.fromMilliseconds(5)), -129.7f));
+
+
+        assertEquals(comparison, uut.getData());
+
+        // Test a bit more complicated.
+        uut = new PressureDataFrame(testdata,
+                                    HighPrecisionDate.fromMinutes(2),
+                                    HighPrecisionTimeSpan.fromSeconds(12));
+
+        comparison.clear();
+        HighPrecisionDate reference = HighPrecisionDate.fromMinutes(2);
+        comparison.add(new DataPoint<>(reference, 0.0f));
+        comparison.add(new DataPoint<>(reference.add(HighPrecisionTimeSpan.fromSeconds(12)), 1.0f));
+        comparison.add(new DataPoint<>(reference.add(HighPrecisionTimeSpan.fromSeconds(24)), 2.0f));
+        comparison.add(new DataPoint<>(reference.add(HighPrecisionTimeSpan.fromSeconds(36)), 3.0f));
+        comparison.add(new DataPoint<>(
+                           reference.add(HighPrecisionTimeSpan.fromSeconds(48)), 77.0f));
+        comparison.add(new DataPoint<>(
+                           reference.add(HighPrecisionTimeSpan.fromSeconds(60)), -129.7f));
+
+        assertEquals(comparison, uut.getData());
+    }
+
+    /**
+     * Tests the constructor that initializes from an already existing data list.
+     */
+    public void testManualConstructor()
+    {
+        PressureDataFrame uut;
+
+        ArrayList<DataPoint<HighPrecisionDate, Float>> testdata = new ArrayList<>();
+        testdata.add(new DataPoint<>(HighPrecisionDate.fromMinutes(12), 109.0f));
+        testdata.add(new DataPoint<>(HighPrecisionDate.fromHours(1), 1.0f));
+        testdata.add(new DataPoint<>(HighPrecisionDate.fromSeconds(1), 0.0f));
+        testdata.add(new DataPoint<>(HighPrecisionDate.fromNanoseconds(1029), -100.0f));
+        testdata.add(new DataPoint<>(HighPrecisionDate.fromMilliseconds(123), 5.0f));
+        testdata.add(new DataPoint<>(HighPrecisionDate.fromMicroseconds(555), 2348951.0f));
+        testdata.add(new DataPoint<>(HighPrecisionDate.fromMicroseconds(555), 512992961.0f));
+        testdata.add(new DataPoint<>(HighPrecisionDate.fromMicroseconds(555), -2139871.0f));
+
+        uut = new PressureDataFrame(testdata);
+        assertEquals(testdata, uut.getData());
+    }
+
+    /**
+     * Tests the constructor that parses the raw data of the remote module. This test automatically
+     * covers the private function parseRawData().
+     */
+    public void testRawDataConstructor()
+    {
+        PressureDataFrame uut;
+        uut = new PressureDataFrame("0,50,30.7,-12.81,33.882,10001221312",
+                                    HighPrecisionDate.epoch,
+                                    HighPrecisionTimeSpan.fromMilliseconds(7));
+
+
+        ArrayList<DataPoint<HighPrecisionDate, Float>> comparison = new ArrayList<>();
+        comparison.add(new DataPoint<>(HighPrecisionDate.epoch, 0.0f));
+        comparison.add(new DataPoint<>(HighPrecisionDate.epoch.add(
+                           HighPrecisionTimeSpan.fromMilliseconds(7)), 50.0f));
+        comparison.add(new DataPoint<>(HighPrecisionDate.epoch.add(
+                           HighPrecisionTimeSpan.fromMilliseconds(14)), 30.7f));
+        comparison.add(new DataPoint<>(HighPrecisionDate.epoch.add(
+                           HighPrecisionTimeSpan.fromMilliseconds(21)), -12.81f));
+        comparison.add(new DataPoint<>(HighPrecisionDate.epoch.add(
+                           HighPrecisionTimeSpan.fromMilliseconds(28)), 33.882f));
+        comparison.add(new DataPoint<>(HighPrecisionDate.epoch.add(
+                           HighPrecisionTimeSpan.fromMilliseconds(35)), 10001221312.0f));
+
+        assertEquals(comparison, uut.getData());
+
+        // Test empty.
+        uut = new PressureDataFrame("",
+                                    HighPrecisionDate.epoch,
+                                    HighPrecisionTimeSpan.fromNanoseconds(1));
+
+        comparison.clear();
+
+        assertEquals(comparison, uut.getData());
+
+        // Test invalid data exception.
+        boolean thrown = false;
+        try
+        {
+            uut = new PressureDataFrame("1290,1290;Invalid",
+                                        HighPrecisionDate.epoch,
+                                        HighPrecisionTimeSpan.fromNanoseconds(1));
+        }
+        catch (NumberFormatException ex)
+        {
+            thrown = true;
+        }
+        assertTrue(thrown);
+
+    }
+}
---
+++ app/src/main/java/tuhh/nme/mp/data/PressureDataFrame.java
@@ -0,0 +1,96 @@
+package tuhh.nme.mp.data;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+
+/**
+ * A data frame that manages the incoming pressure data from remote module.
+ */
+public class PressureDataFrame extends HighPrecisionDatedDataFrame<Float>
+{
+    /**
+     * Instantiates a new PressureDataFrame class and associates the given data with
+     * HighPrecisionDate's.
+     *
+     * @param data The data.
+     * @param time The time the first data point is initialized with and the starting time frame for
+     *             the next points.
+     * @param step The time difference between each data point.
+     */
+    public PressureDataFrame(Collection<Float> data,
+                             HighPrecisionDate time,
+                             HighPrecisionTimeSpan step)
+    {
+        super(data, time, step);
+    }
+
+    /**
+     * Instantiates a new PressureDataFrame class from the given data set.
+     *
+     * @param data A collection of data to initialize with.
+     */
+    public PressureDataFrame(Collection<DataPoint<HighPrecisionDate, Float>> data)
+    {
+        super(data);
+    }
+
+    /**
+     * Instantiates a new PressureDataFrame class from remote module raw data.
+     *
+     * @param raw                    The raw data string from the module. This is a comma separated
+     *                               list of floating-point values.
+     * @param time                   The time the first data point is initialized with and the
+     *                               starting time frame for the next points.
+     * @param step                   The time difference between each data point.
+     * @throws NumberFormatException Thrown when the raw data is corrupted.
+     */
+    public PressureDataFrame(String raw, HighPrecisionDate time, HighPrecisionTimeSpan step)
+    throws NumberFormatException
+    {
+        super(parseRawData(raw), time, step);
+    }
+
+    /**
+     * Parses the raw data incoming from the remote module.
+     *
+     * The pressure data is a comma separated list of floating-point-numbers. For example
+     * "1,2,3,4,3.5,6,0"
+     *
+     * @param raw                    The input data string.
+     * @return                       An array of floats containing the parsed data.
+     * @throws NumberFormatException Thrown when the data format is invalid.
+     */
+    private static List<Float> parseRawData(String raw) throws NumberFormatException
+    {
+
+        ArrayList<Float> data_list = new ArrayList<>();
+
+        if (raw.length() == 0)
+        {
+            // Nothing to parse. Need to catch here since float parsing an empty string throws.
+            // But we want just an empty list.
+            return data_list;
+        }
+
+        String[] data_string_list = raw.split(",");
+
+        data_list.ensureCapacity(data_string_list.length);
+
+        try
+        {
+            for (String elem : data_string_list)
+            {
+                data_list.add(Float.parseFloat(elem));
+            }
+        }
+        catch (NumberFormatException ex)
+        {
+            throw new NumberFormatException("Invalid data format. Parsing a floating-point value " +
+                                            "failed.");
+        }
+
+        return data_list;
+    }
+}
