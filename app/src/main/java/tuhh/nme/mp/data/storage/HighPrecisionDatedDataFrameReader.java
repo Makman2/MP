31ba3929e455317cbdbf9817fef25c109e3652c3
Makman2 <mak95@arcor.de>
2015-05-02 01:27:12 +0200

    storage: Add more specialized reader class
    Reader for HighPrecisionDate'd data entries.

---
+++ app/src/main/java/tuhh/nme/mp/data/storage/HighPrecisionDatedDataFrameReader.java
@@ -0,0 +1,26 @@
+package tuhh.nme.mp.data.storage;
+
+import java.math.BigInteger;
+
+import tuhh.nme.mp.data.HighPrecisionDate;
+
+/**
+ * Base class for all DataFrameReader's with DataFrame's with an x-type of HighPrecisionDate.
+ *
+ * @param <YType> The y-type of the DataFrame.
+ */
+public abstract class HighPrecisionDatedDataFrameReader<YType>
+    extends DataFrameReader<HighPrecisionDate, YType>
+{
+    /**
+     * Converts a byte-chunk to an HighPrecisionDate.
+     * @param data                 The data to convert.
+     * @return                     The HighPrecisionDate represented by the given data chunk.
+     * @throws MPDFFormatException Never thrown.
+     */
+    @Override
+    protected HighPrecisionDate deserializeX(byte[] data) throws MPDFFormatException
+    {
+        return new HighPrecisionDate(new BigInteger(data));
+    }
+}
