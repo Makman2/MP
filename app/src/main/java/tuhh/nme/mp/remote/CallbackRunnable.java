56ac5fc519472e937469bc854661fedbc51ec930
Makman2 <mak95@arcor.de>
2015-05-11 16:19:32 +0200

    threading: Implement callback runnable
    A class that takes a Runnable and a callback and is itself a Runnable.

---
+++ app/src/main/java/tuhh/nme/mp/remote/CallbackRunnable.java
@@ -0,0 +1,37 @@
+package tuhh.nme.mp.remote;
+
+/**
+ * A runnable that allows to define a manual callback.
+ */
+public class CallbackRunnable implements Runnable
+{
+    /**
+     * Instantiates a new CallbackRunnable.
+     *
+     * @param runnable The actual task to perform.
+     * @param callback The callback that shall be invoked after the main task finished.
+     */
+    public CallbackRunnable(Runnable runnable, Runnable callback)
+    {
+        m_Runnable = runnable;
+        m_Callback = callback;
+    }
+
+    /**
+     * Runs the task and then the callback.
+     */
+    public void run()
+    {
+        m_Runnable.run();
+        m_Callback.run();
+    }
+
+    /**
+     * The main task.
+     */
+    private Runnable m_Runnable;
+    /**
+     * The callback.
+     */
+    private Runnable m_Callback;
+}
