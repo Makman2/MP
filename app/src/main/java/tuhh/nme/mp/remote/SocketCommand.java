4be277caadfb9ef0ce6512fc127d6d9a2e3da4ae
Makman2 <mak95@arcor.de>
2015-06-29 14:35:11 +0200

    FIXUP WITH SOCKETCOMMAND

--- app/src/main/java/tuhh/nme/mp/remote/SocketCommand.java
+++ app/src/main/java/tuhh/nme/mp/remote/SocketCommand.java
@@ -28,7 +28,16 @@ public abstract class SocketCommand<T>
      */
     public void run(final Socket socket)
     {
-        m_Result = handle(socket);
+        try
+        {
+            m_Result = handle(socket);
+            m_Exception = null;
+        }
+        catch (Throwable ex)
+        {
+            m_Exception = ex;
+        }
+
         m_ResultLock.unlock();
     }
 
@@ -37,31 +46,51 @@ public abstract class SocketCommand<T>
      *
      * This function blocks until the command has been run.
      *
-     * @return The result of the command.
+     * @return                                The result of the command.
+     * @throws SocketCommandHandlingException Thrown when something went an exception was raised
+     *                                        during handle(). To access this exception, call
+     *                                        getCause() on the exception.
      */
-    public T getResult()
+    public T getResult() throws SocketCommandHandlingException
     {
         m_ResultLock.lock();
-        T result = m_Result;
         m_ResultLock.unlock();
-        return result;
+
+        // The following raises and returns are thread-safe since this object does not access the
+        // used fields asynchronously any more.
+        if (m_Exception != null)
+        {
+            throw new SocketCommandHandlingException(m_Exception);
+        }
+
+        return m_Result;
     }
 
     /**
      * Raised when the command gets handled from the processing network thread.
      *
-     * @param socket The socket to operate on.
-     * @return       The result of the handling. It can be retrieved via getResult() when this
-     *               function finished.
+     * Be sure to catch a null-socket. null will be passed if the command gets processed after the
+     * socket-thread closed.
+     *
+     * @param socket     The socket to operate on. If the socket of the background thread is already
+     *                   closed, the passed value is null.
+     * @return           The result of the handling. It can be retrieved via getResult() when this
+     *                   function finished.
+     * @throws Throwable Any error that occurred during handling. The exception raised will be
+     *                   available under getException().
      */
-    protected abstract T handle(final Socket socket);
+    protected abstract T handle(final Socket socket) throws Throwable;
 
     /**
-     * Locks the result until processing is done.
+     * Locks the result and exception instance until processing is done.
      */
     private final SimpleLock m_ResultLock;
     /**
      * Holds the result.
      */
     private T m_Result;
+    /**
+     * Holds the exception during handle().
+     */
+    private Throwable m_Exception;
 }
